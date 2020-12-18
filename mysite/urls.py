4197b6d32beb66f570faef6070206d8461b355b2
61246880 <jorge.plautz@gmail.com>
2020-12-18 18:16:00 -0300

    Designed Blog data schemma
    close #8

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mysite.blog.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('mysite.blog.urls')),
+    path('blog/', include('mysite.blog.urls', namespace='blog')),
 ]
