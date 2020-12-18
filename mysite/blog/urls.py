25b6a9ef2deac94bc71b7a8dab5c5ed8ff574e57
61246880 <jorge.plautz@gmail.com>
2020-12-18 18:16:00 -0300

    Designed Blog data schemma
    close #8

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
-from mysite.blog.views import home
+from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', home, name='home'),
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
 
 
 def home(request):
-    return HttpResponse('Olá Blog')
+    return HttpResponse('<h1>Welcome to AntMele Blog -> /blog</h1>')
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
